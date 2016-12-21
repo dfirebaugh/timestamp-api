83af0e5a0b0a27944d318b29b1d5e13e7ec9ac1d
Dustin Firebaugh <dafirebaugh@gmail.com>
2016-12-21 17:02:39 +0000

    added timestamp code

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
 var express = require('express')
 var app = express()
+var moment = require('moment')
+var strftime = require('strftime')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var port = process.env.PORT || 8800
+// var createJsonRes
+
+app.get('/', function(req,res){
+    var dateStr = req.params.date
+    
+    var resObj = {natural:null, unix:null}
+    
+    if(isNaN(Number(dateStr))){
+        var d = moment(dateStr)
+        if(d.isValid()){
+            resObj = createJsonRes(d.toDate())
+        }
+    }
+    else{
+        var d = moment.unix(Number(dateStr));
+        resObj = createJsonRes(d.toDate())
+    }
+    
+    res.json(resObj)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function(){
+    console.log("Listening on port: " + port)
 })
+
+
+
+
+function createJsonRes(date){
+    return{
+        natural: formatDate(date),
+        unix: date.getTime() / 1000
+    }
+}
+
+function formatDate(date){
+    return strftime("%B %d, %Y",date)
+}
+
+
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
